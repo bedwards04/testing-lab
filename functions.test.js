831739e7f3e3c81be52ebebddb5ef1c04a8386c4
Brendan Edwards <bdawg@Brendans-MacBook-Air.local>
2021-06-22 16:07:07 -0600

    adding math functions and tests

--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,5 @@
 const { test, describe } = require('@jest/globals');
-const { returnTwo, greeting, add } = require('./functions');
+const { returnTwo, greeting, add, multiply, divide, subtract } = require('./functions');
 
 test('returnTwo should return the integer 2', function() {
     expect(returnTwo()).toBe(2);
@@ -15,8 +15,20 @@ describe('Greeting tests', function() {
 });
 
 
-test('add test', function() {
-    expect(add(1, 2)).toEqual(3);
-    expect(add(5, 9)).toBe(14);
-});
 
+
+describe('Math functions', function() {
+    test('Add test', function() {
+        expect(add(1, 2)).toEqual(3);
+        expect(add(5, 9)).toBe(14);
+    });
+    test('Multiply test', function() {
+        expect(multiply(6, 6)).toEqual(36);
+    });
+    test('Divide test', function() {
+        expect(divide(20, 2)).toEqual(10);
+    });
+    test('Subtract test', function() {
+        expect(subtract(40, 25)).toEqual(15);
+    });
+});
